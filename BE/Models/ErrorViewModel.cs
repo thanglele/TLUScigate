15a4b4ee844c1a4d5d8496be152b2f0e8f12a890
Anhhieu2702 <vungochieu0999@gmail.com>
2025-03-14 14:32:39 +0700

    config by LeThang

--- BE/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OAuthv2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
